f2e2b331075e423cab1d6955e6f8b41a81c66a5b
likai <likai@91xft.cn>
2021-12-09 18:05:50 +0800

    update

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-souUser=root #服务端创建的用户名
-souPwd=jianc@0817 #服务端创建的密码
+souUser=ercili #服务端创建的用户名
+souPwd=a4321469 #服务端创建的密码
 souIP=119.23.147.29 #服务端ip address
 souDir=/Users/ericli/Documents/xft-node-work/ideploy/temp/likai/23/dist #服务端的路径
 desDir=/likai/dist #客户端的路径
 auto_rsync () {
     expect -c "set timeout -1;
-                spawn rsync -av --delete $4 $2@$3:$5;
+                spawn rsync -av --delete $2@$3:$4 $5;
                 expect {
                     *assword:* {send -- $1\r;
-                                 expect {'
+                                 expect {
                                     *denied* {exit 2;}
                                     eof
                                  }
