f0ea9f0bd6b35a68c6c5cd87cd0fb4853d582b34
likai <likai@91xft.cn>
2021-12-09 15:25:35 +0800

    update

--- src/common/service/impl/deploy.sh
+++ src/common/service/impl/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-souUser=$1 #服务端创建的用户名
-souPwd=$2 #服务端创建的密码
-souIP=$4 #服务端ip address
-souDir=$3 #服务端的路径
-desDir=$6 #客户端的路径
+souUser=1 #服务端创建的用户名
+souPwd=2 #服务端创建的密码
+souIP=172.18.254.200 #服务端ip address
+souDir=/data/www/ci-server/temp/likai/24/dist #服务端的路径
+desDir=/data/www/91xft-xiangyasan-admin #客户端的路径
 auto_rsync () {
     expect -c "set timeout -1;
                 spawn rsync -av --delete $4 $3:$5;
