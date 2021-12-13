210b8a1cdae04977eee9fbc503450e1bf48446d2
likai <likai@91xft.cn>
2021-12-13 09:59:42 +0800

    update

--- src/common/service/impl/build_javascript.sh
+++ src/common/service/impl/build_javascript.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 export PATH=/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/soft/node-v4.4.4-linux-x64/bin:/root/bin:/Users/boutell/npm/bin
-modulesPath=$5/temp/mods/$3/
+# modulesPath=$5/temp/mods/$3/
 projectPath=$1
 task=$2
-# pwd
-
-# if [ "$4" -eq "2" ]; then
-#   rm -rf $modulesPath
-# fi
-# echo $modulesPath
-# if [ ! -d $modulesPath ]; then
-#   mkdir -p $modulesPath
-# fi
-
-# cp -f $projectPath/package.json $modulesPath
-
-# cd $modulesPath
 
 #更新依赖库
 
@@ -28,10 +15,6 @@ if [ "$4" -eq "2" ]; then
 cnpm install --save
 fi
 echo 'npm installed'
-
-# echo '创建node_modules软连接  ln -s  ${modulesPath}/node_modules node_modules ---'
-# ln -s ../../mods/$3/node_modules ./node_modules
-pwd
 
 echo '开始构建'
 
