34f90e3638c016e3bd1c49a4aff37e3b3ad91b86
likai <likai@91xft.cn>
2021-12-13 09:53:53 +0800

    update

--- src/common/service/impl/build_javascript.sh
+++ src/common/service/impl/build_javascript.sh
@@ -3,34 +3,34 @@ export PATH=/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr
 modulesPath=$5/temp/mods/$3/
 projectPath=$1
 task=$2
-pwd
+# pwd
 
-if [ "$4" -eq "2" ]; then
-  rm -rf $modulesPath
-fi
-echo $modulesPath
-if [ ! -d $modulesPath ]; then
-  mkdir -p $modulesPath
-fi
+# if [ "$4" -eq "2" ]; then
+#   rm -rf $modulesPath
+# fi
+# echo $modulesPath
+# if [ ! -d $modulesPath ]; then
+#   mkdir -p $modulesPath
+# fi
 
-cp -f $projectPath/package.json $modulesPath
+# cp -f $projectPath/package.json $modulesPath
 
-cd $modulesPath
+# cd $modulesPath
 
 #更新依赖库
-
-if [ "$4" -eq "2" ]; then
-cnpm install --save
-fi
-echo 'npm installed'
 
 cd ../../../
 pwd
 
 cd $projectPath
 pwd
-echo '创建node_modules软连接  ln -s  ${modulesPath}/node_modules node_modules ---'
-ln -s ../../mods/$3/node_modules ./node_modules
+if [ "$4" -eq "2" ]; then
+cnpm install --save
+fi
+echo 'npm installed'
+
+# echo '创建node_modules软连接  ln -s  ${modulesPath}/node_modules node_modules ---'
+# ln -s ../../mods/$3/node_modules ./node_modules
 pwd
 
 echo '开始构建'
